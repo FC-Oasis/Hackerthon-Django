5541f987838e0032253376e96dc630a3db61121e
Ahyeon <ahyeoncode@gmail.com>
2018-07-20 18:16:50 +0900

    [FIX] STATIC_DIR, MEDIA_ROOT 수정, static/image생성

--- app/config/views.py
+++ app/config/views.py
@@ -5,18 +5,8 @@ from django.shortcuts import render
 
 from observation.models import Record
 
-#
-# def index(request):
-#     return render(request, 'main.html')
-
 
 def index(request):
-    Record.objects.create(
-        humidity=40,
-        temperature=29,
-        recorded_at=datetime.datetime.strptime('2018-07-20T16:00:36', '%Y-%m-%dT%H:%M:%S'),
-    )
-
     data = Record.objects.first()
     context = {
         'data': data,

e5aa0dc1dc3826c93f56e04b07076bf2ad3c8554
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-20 11:34:53 +0900

    [ADD] Record 모델 추가 #1

--- app/observation/models.py
+++ app/observation/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+
+class Record(models.Model):
+    humidity = models.IntegerField()
+    temperature = models.IntegerField()
+    recorded_at = models.DateTimeField(
+        unique=True,
+    )
+
+    class Meta:
+        ordering = ('-recorded_at',)
